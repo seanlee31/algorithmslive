a352a562c292936ae2b3c6a0917ea2c7c1fcb04c
Sean Lee <sean85326@hotmail.com>
2018-11-12 18:00:10 -0500

    [NEW] Setup webpack, babel, react environment.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: './src/index.js',
+   module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+   output: {
+     path: path.join(__dirname, '/bundle'),
+     publicPath: '/',
+     filename: 'bundle.js'
+   },
+   plugins: [
+      new webpack.HotModuleReplacementPlugin()    // Auto Refresh Changes
+      // new HtmlWebpackPlugin({
+      //    template: './dist/index.html'
+      // })
+   ],
+   devServer: {
+     contentBase: './dist',
+     port: 8080,
+     hot: true
+   }
+ };
